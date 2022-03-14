6003d951689a332a777f864dd82d6d070c6c64ef
Redhu Sunny <sunny.redhu@nokia.com>
2022-03-14 07:20:52 +0530

    Add error messages and background to frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,9 @@ import './App.css';
 import Button from 'react-bootstrap/Button';
 import Table from 'react-bootstrap/Table';
 import Spinner from 'react-bootstrap/Spinner';
+// import Card from 'react-bootstrap/Card';
+import Toast from 'react-bootstrap/Toast';
+import ToastContainer from 'react-bootstrap/ToastContainer';
 const axios = require('axios');
 
 
@@ -20,36 +23,68 @@ const urls = [
 function App() {
   const [rows, setRows] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [showError, setShowError] = useState(false);
+  const [errorMsg, setErrorMsg] = useState("");
 
   return (
     <div className="App">
-      <Table striped bordered hover>
-        <thead>
-          <tr>
-            <th>URL</th>
-            <th>US</th>
-            <th>Europe</th>
-            <th>Asia</th>
-          </tr>
-        </thead>
-        <tbody>
-          {rows}
-        </tbody>
-      </Table>
-      <Button onClick={() => getRows(setRows, setIsLoading)} variant="primary">Test</Button>
-      <br /><br />
-      {isLoading ? <Spinner animation="grow" variant="primary" /> : null}
+      <div className='background'></div>
+      <div className='foreground'>
+        {/* <Card style={{ width: '100%' }}>
+          <Card.Body>
+            <Card.Title>TRIVAGO AND COMPETITORS</Card.Title>
+            <Card.Subtitle className="mb-2 text-muted">Latency = T_response - T_request</Card.Subtitle>
+            <Card.Text>
+              <li>I have compared latencies of some applications related to the Travel industry.</li>
+              <li>The data is sorted according to average of the 3 regions.</li>
+              <li>All the measurements are in seconds.</li>
+            </Card.Text>
+          </Card.Body>
+        </Card> */}
+
+        <Table class="table" striped bordered hover>
+          <thead>
+            <tr>
+              <th>URL</th>
+              <th>US</th>
+              <th>Europe</th>
+              <th>Asia</th>
+            </tr>
+          </thead>
+          <tbody>
+            {rows}
+          </tbody>
+        </Table>
+        <Button onClick={() => getRows(setRows, setIsLoading, setShowError, setErrorMsg)} variant="primary">Test</Button>
+        <br /><br />
+        {isLoading ? <Spinner animation="grow" variant="primary" /> : null}
+
+        <ToastContainer className="p-3" position="bottom-center">
+          <Toast onClose={() => setShowError(!showError)} show={showError}>
+            <Toast.Header>
+              <img
+                src="holder.js/20x20?text=%20"
+                className="rounded me-2"
+                alt=""
+              />
+              <strong className="me-auto">ERROR</strong>
+            </Toast.Header>
+            <Toast.Body>{errorMsg}. Please try again after sometime!</Toast.Body>
+          </Toast>
+        </ToastContainer>
+      </div>
     </div>
   );
 }
 
-const getRows = async (setRows, setIsLoading) => {
+const getRows = async (setRows, setIsLoading, setShowError, setErrorMsg) => {
   const requests = [
     axios.get(`${process.env.REACT_APP_US_SERVER_URL}/testspeed?urls=${urls.join()}`),
     axios.get(`${process.env.REACT_APP_EUROPE_SERVER_URL}/testspeed?urls=${urls.join()}`),
     axios.get(`${process.env.REACT_APP_ASIA_SERVER_URL}/testspeed?urls=${urls.join()}`)
   ]
   try {
+    setShowError(false)
     setIsLoading(true)
     const [responseUS, responseEurope, responseAsia]  = await axios.all(requests)
 
@@ -74,6 +109,8 @@ const getRows = async (setRows, setIsLoading) => {
     ))
   } catch(err) {
     console.log(err)
+    setErrorMsg(err.message)
+    setShowError(true)
   } finally {
     setIsLoading(false)
   }
